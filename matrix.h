afa105c53b4c3a0851efef09043dd30843a27dd5
1amthebest1 <165074489+1amthebest1@users.noreply.github.com>
2026-05-07 02:00:44 +0300

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -10,17 +10,17 @@
 //Implement a Matrix Class Template
 
 //Your matrix must support :
-//• Dynamic memory allocation
-//• Any numeric data type(T) [done!?]
-//• Row - major storage [okay i will do it]
+//â€¢ Dynamic memory allocation
+//â€¢ Any numeric data type(T) [done!?]
+//â€¢ Row - major storage [okay i will do it]
 //Required Operations :
-//• Constructor : Matrix(rows, cols)
-//• Copy constructor & destructor
-//• Element access using operator()(i, j)
-//• Matrix addition& subtraction
-//• Scalar multiplication
-//• Matrix multiplication(if dimensions allow)
-//• Transpose
+//â€¢ Constructor : Matrix(rows, cols)
+//â€¢ Copy constructor & destructor
+//â€¢ Element access using operator()(i, j)
+//â€¢ Matrix addition& subtraction
+//â€¢ Scalar multiplication
+//â€¢ Matrix multiplication(if dimensions allow)
+//â€¢ Transpose
 
 using namespace std;
 
@@ -34,7 +34,6 @@ public:
 	T buttonPositionY[30][30];
 
 	int Iterator = 0;
-	int opacity = 0;
 	bool musicPlaying = false;
 	bool SingleMode = false;
 
@@ -47,34 +46,34 @@ public:
 
 	Color checkColorValue(T colorValue) {
 
-		if (opacity > 255) {
-			opacity = 0;
+		if (opacity_ > 255) {
+			opacity_ = 0;
 		}
 
-		if (opacity < 0) {
-			opacity = 255;
+		if (opacity_ < 0) {
+			opacity_ = 255;
 		}
 
 		if (colorValue == 0) {
-			return Color{ 255, 0, 13, opacity}; //red
+			return Color{ 255, 0, 13, static_cast<unsigned char>(opacity_)}; //red
 		}
 		else if (colorValue == 1) {
-			return Color{ 0, 0, 200, opacity }; //blue
+			return Color{ 0, 0, 200, static_cast<unsigned char>(opacity_) }; //blue
 		}
 		else if (colorValue == 2) {
-			return Color{ 255, 255, 255, opacity }; //white
+			return Color{ 255, 255, 255, static_cast<unsigned char>(opacity_) }; //white
 		}
 		else if (colorValue == 3) {
-			return Color{ 128, 0, 128, opacity}; //purple
+			return Color{ 128, 0, 128, static_cast<unsigned char>(opacity_) }; //purple
 		}
 		else if (colorValue == 4) {
-			return Color{ 128, 128, 128, opacity };	//gray
+			return Color{ 128, 128, 128, static_cast<unsigned char>(opacity_) };	//gray
 		}
 		else if (colorValue == 5) {
-			return Color{ 255, 255, 255, opacity };	//white
+			return Color{ 255, 255, 255, static_cast<unsigned char>(opacity_) };	//white
 		}
 		else {
-			return Color{ 255, 165, 0, opacity }; //orange
+			return Color{ 255, 165, 0, static_cast<unsigned char>(opacity_) }; //orange
 		}
 	}
 
@@ -352,6 +351,50 @@ public:
 				if (mousePoint.y >= 840 && mousePoint.y <= 840 + 40) {
 					grayScale(buttonColor);
 					cout << "called Load Save Image" << endl;
+				}
+			}
+		}
+	}
+
+	void increaseOpacity() {
+		opacity_+=17;
+		cout << "increase Opacity Called" << endl;
+	}
+
+	void drawIncreaseOpacity() {
+		DrawRectangle(600, 770, 90, 40, YELLOW);
+		DrawText("Brighten", 605, 780, 15, BLACK);
+	}
+
+	void checkIncreaseOpacityPressed() {
+		if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+			Vector2 mousePoint = GetMousePosition();
+			if (mousePoint.x >= 600 && mousePoint.x <= 650 + 90) {
+				if (mousePoint.y >= 770 && mousePoint.y <= 770 + 40) {
+					increaseOpacity();
+					cout << "called Increase Opacity" << endl;
+				}
+			}
+		}
+	}
+
+	void decreaseOpacity() {
+		opacity_-=17;
+		cout << "decreaseOpacity Called" << endl;
+	}
+
+	void drawDecreaseOpacity() {
+		DrawRectangle(600, 830, 90, 40, YELLOW);
+		DrawText("Dim", 605, 840, 15, BLACK);
+	}
+
+	void checkDecreaseOpacityPressed() {
+		if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+			Vector2 mousePoint = GetMousePosition();
+			if (mousePoint.x >= 600 && mousePoint.x <= 650 + 90) {
+				if (mousePoint.y >= 830 && mousePoint.y <= 830 + 40) {
+					decreaseOpacity();
+					cout << "called Increase Opacity" << endl;
 				}
 			}
 		}
